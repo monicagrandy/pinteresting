d2a971c82b99a32f1c82d0de6cee0591b8e4a840
Monica Grandy <monica.grandy@gmail.com>
2015-07-27 08:22:58 -0700

    implemented devise for users

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
   root "pages#home"
 
   get "about" => "pages#about"
 
-  get "login" => "pages#login"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".

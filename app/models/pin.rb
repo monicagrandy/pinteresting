da164b0931cb48f2dab6e1a491272d2b1825fa7c
Monica Grandy <monica.grandy@gmail.com>
2015-08-02 18:33:46 -0700

    added name field

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -2,4 +2,6 @@ class Pin < ActiveRecord::Base
 	belongs_to :user
 	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
 	validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"] 
+	validates :description, presence: true
+	validates :image, presence: true 
 end
